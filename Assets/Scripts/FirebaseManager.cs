f7ae6388babc2ade0501c19a5255e570313fd708
LS1653 <estebanpuerta2006@gmail.com>
2026-09-22 16:31:00 -0500

    Initialize Firebase Authentication

---
+++ Assets/Scripts/FirebaseManager.cs
@@ -0,0 +1,40 @@
+using Firebase;
+using Firebase.Auth;
+using Firebase.Extensions;
+using UnityEngine;
+
+public class FirebaseManager : MonoBehaviour
+{
+    private FirebaseApp app;
+    private FirebaseAuth auth;
+
+    private void Start()
+    {
+        CheckFirebaseDependencies();
+    }
+
+    private void CheckFirebaseDependencies()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync()
+            .ContinueWithOnMainThread(task =>
+            {
+                DependencyStatus dependencyStatus = task.Result;
+
+                if (dependencyStatus == DependencyStatus.Available)
+                {
+                    app = FirebaseApp.DefaultInstance;
+                    auth = FirebaseAuth.DefaultInstance;
+
+                    Debug.Log("Firebase inicializado correctamente.");
+                    Debug.Log("Firebase Authentication inicializado correctamente.");
+                }
+                else
+                {
+                    Debug.LogError(
+                        "No se pudieron resolver las dependencias de Firebase: "
+                        + dependencyStatus
+                    );
+                }
+            });
+    }
+}
